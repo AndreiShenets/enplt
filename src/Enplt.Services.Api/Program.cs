891f1f67cada78110c9ed65793251b2c7bbf1d6f
Andrei Shenets <shenets.andrei@gmail.com>
2025-02-01 10:15:50 +0100

    Removed not required

--- src/Enplt.Services.Api/Program.cs
+++ src/Enplt.Services.Api/Program.cs
@@ -2,17 +2,12 @@ using System.Net;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
-
 // As port 3000 is a hard requirement, I am hardcoding it here to not deal with appsettings.json and environment variables
 builder.WebHost.ConfigureKestrel((_, serverOptions) => serverOptions.Listen(IPAddress.Loopback, 3000));
 
 
 WebApplication app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapPost("/calendar/query", context => context.Response.WriteAsync("Hello World!"));
 
 await app.RunAsync();
