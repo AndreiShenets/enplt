224a1164a0a7c352a243f7e1ebcf4465c1986ac9
Andrei Shenets <shenets.andrei@gmail.com>
2025-02-01 15:05:32 +0100

    Implemented first version of the slot calculation algorithm

--- src/Enplt.Services.Api/SaleManagerAvailability/SaleManagerAvailabilityQuery.cs
+++ src/Enplt.Services.Api/SaleManagerAvailability/SaleManagerAvailabilityQuery.cs
@@ -1,6 +1,9 @@
 ﻿using Enplt.Services.Api.Database.Entities;
 using Enplt.Services.Api.Domain;
 
+using ManagerCount = int;
+using SaleManagerId = int;
+
 namespace Enplt.Services.Api.SaleManagerAvailability;
 
 public sealed class SaleManagerAvailabilityQuery
@@ -29,8 +32,60 @@ public sealed class SaleManagerAvailabilityQuery
                 cancellationToken
             );
 
-        return [];
+        Dictionary<SaleManagerId, CalendarSlotEntity[]> bookedSlotsByManager =
+            slotEntities
+                .Where(slot => slot.Booked)
+                .GroupBy(slot => slot.SaleManagerId)
+                .ToDictionary(
+                    group => group.Key,
+                    group => group.ToArray()
+                );
+
+        Dictionary<SlotRange, ManagerCount> slotRanges = new();
+
+        foreach (CalendarSlotEntity slot in slotEntities)
+        {
+            if (slot.Booked)
+            {
+                continue;
+            }
+
+            if (!bookedSlotsByManager.TryGetValue(slot.SaleManagerId, out CalendarSlotEntity[]? managerBookedSlots))
+            {
+                managerBookedSlots = [];
+            }
+
+            bool slotAlreadyBooked =
+                managerBookedSlots.Any(
+                    mbs =>
+                        (mbs.From <= slot.From && slot.From <= mbs.To)
+                        || (mbs.From <= slot.To && slot.To <= mbs.To)
+                );
+
+            if (!slotAlreadyBooked)
+            {
+                SlotRange slotRange = new (slot.From, slot.To);
+                slotRanges.TryGetValue(slotRange, out ManagerCount count);
+                ++count;
+                slotRanges[slotRange] = count;
+            }
+        }
+
+        List<Availability> availabilities =
+            slotRanges.Select(
+                slotRange =>
+                    new Availability
+                    {
+                        StartDate = slotRange.Key.From,
+                        AvailableCount = slotRange.Value
+                    }
+            )
+            .ToList();
+
+        return availabilities;
     }
+
+    private record struct SlotRange(DateTimeOffset From, DateTimeOffset To);
 }
 
 public sealed class Availability
